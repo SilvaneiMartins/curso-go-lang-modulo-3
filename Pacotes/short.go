af6c34ffce1e72ef23daae340e37054a545698f0
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-12-22 10:51:44 -0300

    Exercicio sort do curso go lang

---
+++ Pacotes/short.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Dados struct {
+	Nome  string
+	Idade int
+}
+
+type ParaNome []Dados
+
+func (ps ParaNome) Len() int {
+	return len(ps)
+}
+
+func (ps ParaNome) Less(i, j int) bool {
+	return ps[i].Nome < ps[j].Nome
+}
+
+func (ps ParaNome) Swap(i, j int) {
+	ps[i], ps[j] = ps[j], ps[i]
+}
+
+func main() {
+	criancas := []Dados{
+		{"João", 20},
+		{"Maria", 19},
+		{"José", 21},
+	}
+
+	sort.Sort(ParaNome(criancas))
+
+	fmt.Println(criancas)
+}
